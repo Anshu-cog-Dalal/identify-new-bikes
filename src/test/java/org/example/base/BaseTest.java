10dd1506c753fc6fc467bbdea36781f72f6fedb7
Anshu-cog-Dalal <anshukumar.dalal@cognizant.com>
2026-04-21 11:17:16 +0530

    Excel Sheet Added

--- src/test/java/org/example/base/BaseTest.java
+++ src/test/java/org/example/base/BaseTest.java
@@ -1,4 +1,18 @@
 package org.example.base;
-
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+public class BaseTest{
+    protected WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        DriverFactory.initDriver();
+        driver = DriverFactory.getDriver();
+        System.out.println("Setup complete — browser is ready!");
+    }
+    @AfterClass
+    public void tearDown(){
+        DriverFactory.quitDriver();
+        System.out.println("Teardown complete — browser is closed!");
+    }
 }
