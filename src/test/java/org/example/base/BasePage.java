a4fdec7e3c5518d042356b6c8781e7a74a8955db
souravpetal24 <sourav.petal@cognizant.com>
2026-04-21 10:30:13 +0530

    Click the code

--- src/test/java/org/example/base/BasePage.java
+++ src/test/java/org/example/base/BasePage.java
@@ -1,4 +1,29 @@
 package org.example.base;
-
-public class BasePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+public class BasePage{
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+    protected void clickElement(WebElement element){
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+    protected void typeText(WebElement element,String text){
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(text);
+    }
+    protected String getElementText(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+        return element.getText();
+    }
 }
