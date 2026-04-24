54ec8d107b6cff77911252ef9ec9ece26a32e6c6
Bhowmik <2479749@cognizant.com>
2026-04-24 10:41:05 +0530

    Used cars

--- src/test/java/org/example/pages/UsedCarsPage.java
+++ src/test/java/org/example/pages/UsedCarsPage.java
@@ -1,4 +1,42 @@
 package org.example.pages;
 
-public class UsedCarsPage {
+import org.example.base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsedCarsPage extends BasePage {
+    public UsedCarsPage(WebDriver driver){
+        super(driver);
+    }
+    @FindBy(xpath =  "//*[@id=\"popularCityList\"]/li[7]/a")
+    private WebElement chennaiCity;
+
+    @FindBy(xpath="//div[@class='gsc_thin_scroll']/ul/li/label")
+    private List<WebElement> popularCarList;
+
+    public void selectCity(){
+        clickElement(chennaiCity);
+        System.out.println("City Clicked");
+    }
+    public List<String> getPopularList(){
+        List<String> carNames = new ArrayList<String>();
+        for(WebElement car:popularCarList){
+            String name = car.getText().trim();
+            carNames.add(name);
+        }
+        return carNames;
+    }
+    public void displayPopularUsedCars(){
+        List<String> cars = getPopularList();
+        System.out.println("Popular used cars in Chennai:");
+        for(String car:cars){
+            System.out.println(car);
+        }
+    }
+
+
 }
--- src/test/java/org/example/tests/UsedCarsTest.java
+++ src/test/java/org/example/tests/UsedCarsTest.java
@@ -1,4 +1,19 @@
 package org.example.tests;
 
-public class UsedCarsTest {
+import org.example.base.BaseTest;
+import org.example.pages.HomePage;
+import org.example.pages.UsedCarsPage;
+import org.testng.annotations.Test;
+
+public class UsedCarsTest extends BaseTest {
+    @Test
+    public void verifyUsedCars(){
+        HomePage homePage = new HomePage(driver);
+        homePage.cilckUsedCar();
+        UsedCarsPage usedCarsP = new UsedCarsPage(driver);
+        usedCarsP.selectCity();
+        usedCarsP.displayPopularUsedCars();
+
+    }
+
 }
